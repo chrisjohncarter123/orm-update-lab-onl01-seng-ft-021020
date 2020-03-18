25648a5c632a6f84cdc74b539350d699f1714023
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-18 03:34:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,12 @@ class Student
     end
   end
   
+  def create(name, grade)
+    new_student = self.new(name, grade)
+    
+    new_student.save
+  end
+  
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
