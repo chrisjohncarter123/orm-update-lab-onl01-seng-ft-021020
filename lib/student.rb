50659105fb2b44d141f15a770f3f2b12725eccbc
Christopher Carter <chrisjohncarter123@gmail.com>
2020-03-18 03:38:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,6 +64,13 @@ class Student
   def self.new_from_db(row)
     new_student = self.new(row[1], row[2], row[0])
   end
+  
+  def self.find_by_name(name)
+    sql =  <<-SQL
+      SELECT * FROM students WHERE (?) = name
+        SQL
+    DB[:conn].execute(sql)[0]
+  end
 
 
 end
